70a020279b1b02892e89b757dc93cbdec9b222e2
Veronica <veronica@MacBookAir.home>
2025-05-10 22:21:04 +0200

    Set up project

---
+++ src/main/java/com/example/OrderPaymentOptimizer/OrderPaymentOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderPaymentOptimizer;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class OrderPaymentOptimizerApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(OrderPaymentOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OrderPaymentOptimizer/OrderPaymentOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrderPaymentOptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
